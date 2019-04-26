048a134b27a873acf633247341f9d7056393d69c
Romain de Wolff <rdewolff@gmail.com>
2019-04-26 14:57:48 +0200

    Added iterm2 command

--- install.sh
+++ install.sh
@@ -9,3 +9,10 @@ ln -s ~/Projects/dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/Projects/dotfiles/zsh/.zshrc ~/.zshrc
 
 ln -s ~/Projects/dotfiles/franz/recipes/dev ~/Library/Application\ Support/Franz/recipes
+
+# iterm2
+# cf : http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
+
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/Projects/dotfiles/iterm2"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
