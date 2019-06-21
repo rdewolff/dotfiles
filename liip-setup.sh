8ca312be7779d0379ddaa1ab2383ae3f50e05b70
Romain de Wolff <rdewolff@gmail.com>
2019-06-21 10:11:05 +0200

    Added liip taxi install

---
+++ liip-setup.sh
@@ -0,0 +1,8 @@
+
+# install taxi for time tracking
+# tutorial https://wiki.liip.ch/pages/viewpage.action?pageId=611125476
+sudo easy_install pip
+pip install --user taxi
+pip install --user taxi-zebra
+# launch the taxi setup 
+taxi update
